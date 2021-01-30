6aeee6ac69858ec493fbe4b36c23adb898f3d37c
leandrohorizon <leandro.mcontato@gmail.com>
2021-01-29 23:56:46 -0300

    first day complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json';
+import { theme } from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = db.theme;
+//const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
